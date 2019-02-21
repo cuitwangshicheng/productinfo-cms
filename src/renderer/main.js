b5bdeccaecbfe352bc63c1835218f904c3d53fc9
cuitwangshicheng <775799488@qq.com>
2019-02-21 17:41:04 +0800

    移除无用组件

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 
 import App from './App'
 import router from './router'
-import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 const MongodbModel = require('./store/mongodb')
@@ -22,6 +21,5 @@ Vue.use(ElementUI)
 new Vue({
   components: { App },
   router,
-  store,
   template: '<App/>'
 }).$mount('#app')
